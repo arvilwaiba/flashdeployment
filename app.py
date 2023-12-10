5e2427bc4fc08f64edd7dca649484fa5a7b74037
arvilwaiba <48643409+arvilwaiba@users.noreply.github.com>
2023-12-10 12:57:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
 import re
 import traceback
+import logging
 from flask import Flask, request, jsonify, Response, json
 from flask_cors import CORS
 from flask_ngrok import run_with_ngrok
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 # Download stopwords
 try:
@@ -30,38 +31,43 @@ run_with_ngrok(app)  # Start ngrok when the app is run
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 model_file_path = os.path.join(current_dir, 'model.h5')
-tokenizer = None  # Tokenizer will be created dynamically during app initialization
-model = None
+
+try:
+    model = keras.models.load_model(model_file_path)
+except keras.utils.HDF5MatrixError as e:
+    logging.error(f"Error loading the model: {e}")
+    raise SystemExit(1)  # Exit the application if model loading fails
 
 max_words = 10000
 max_len = 100
+tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
 stop_words = set(stopwords.words('english'))
 
 def preprocess_text(text):
+    logging.info("Original Text: %s", text)
+    
+    # Check for empty input
+    if not text:
+        logging.warning("Empty input text")
+        return None, None
+    
     # Convert to lowercase
     text = text.lower()
     # Remove special characters, numbers, and punctuation
     text = re.sub(r'[^a-zA-Z\s]', '', text)
-    # Tokenize using nltk.word_tokenize
+    # Tokenize
     words = word_tokenize(text)
     # Remove stopwords
     words = [word for word in words if word not in stop_words]
-    return ' '.join(words)
-
-def initialize_models():
-    global model, tokenizer
-    # Load the sentiment analysis model
-    model = keras.models.load_model(model_file_path)
-
-    # Dummy training data (replace this with your actual training data)
-    train_data = ["This is a positive sentence.", "This is a negative sentence.", "This is a neutral sentence."]
     
-    # Create and fit the tokenizer
-    tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
-    tokenizer.fit_on_texts(train_data)
-
-# Initialize models when the app starts
-initialize_models()
+    # Check if there are valid words after removing stopwords
+    if not words:
+        logging.warning("No valid words after removing stopwords")
+        return None, None
+    
+    preprocessed_text = ' '.join(words)
+    logging.info("Preprocessed Text: %s", preprocessed_text)
+    return preprocessed_text, words
 
 @app.route('/predict_sentiment', methods=['POST'])
 def predict_sentiment_route():
@@ -69,17 +75,25 @@ def predict_sentiment_route():
         data = request.get_json(force=True)
         text = data.get('text', '')
 
-        preprocessed_text = preprocess_text(text)
+        preprocessed_text, words = preprocess_text(text)
 
-        if not preprocessed_text:
-            print("Error: Empty preprocessed text")
-            return jsonify({'error': 'Empty preprocessed text'}), 400
+        if preprocessed_text is None:
+            return jsonify({'error': 'Empty or invalid input text'}), 400
 
-        # Tokenize and pad sequence
+        logging.info("Preprocessed Text: %s", preprocessed_text)
+
+        if not words:
+            return jsonify({'error': 'No valid words after removing stopwords'}), 400
+
         sequence = tokenizer.texts_to_sequences([preprocessed_text])
-        padded_sequence = pad_sequences(sequence, maxlen=max_len, padding='post', truncating='post')
 
-        print("Padded Sequence:", padded_sequence)
+        if not sequence or not sequence[0]:
+            return jsonify({'error': 'Unable to tokenize the input text'}), 400
+
+        logging.info("Tokenized Sequence: %s", sequence)
+
+        padded_sequence = pad_sequences(sequence, maxlen=max_len, padding='post', truncating='post')
+        logging.info("Padded Sequence: %s", padded_sequence)
 
         prediction = model.predict(padded_sequence)
         predicted_label = np.argmax(prediction)
@@ -87,18 +101,10 @@ def predict_sentiment_route():
         sentiment_mapping = {0: 'negative', 1: 'neutral', 2: 'positive'}
         predicted_sentiment = sentiment_mapping[predicted_label]
 
-        print("Predicted Sentiment:", predicted_sentiment)
-
-        response = Response(response=json.dumps({'sentiment': predicted_sentiment}),
-                            status=200,
-                            mimetype="application/json")
-
-        print("Entire Response:", response.response)
-
-        return response
+        return jsonify({'sentiment': predicted_sentiment})
 
     except Exception as e:
-        print("Exception:", e)
+        logging.error("Exception: %s", e)
         traceback.print_exc()
         return jsonify({'error': 'Internal Server Error'}), 500
 
